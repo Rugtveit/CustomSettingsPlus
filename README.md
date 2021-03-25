04166b4b5e570ae98cc8ba2fe627e4840197bdbd
Lillie <lillierugtveit@gmail.com>
2021-03-25 10:41:09 +0100

    Added project template

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CustomSettingsPlus
-Beat Saber mod that allows you to customize the game to your liking.
+
+CustomSettingsPlus allows you to change a lot more in the game
+
+## Credits
+
+* [zoller27osu](https://github.com/zoller27osu), [Sc2ad](https://github.com/Sc2ad) and [jakibaki](https://github.com/jakibaki) - [beatsaber-hook](https://github.com/sc2ad/beatsaber-hook)
+* [raftario](https://github.com/raftario) 
+* [Lauriethefish](https://github.com/Lauriethefish) and [danrouse](https://github.com/danrouse) for [this template](https://github.com/Lauriethefish/quest-mod-template)
---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include "main.hpp"
+
+static ModInfo modInfo; 
+
+
+Configuration& getConfig() {
+    static Configuration config(modInfo);
+    config.Load();
+    return config;
+}
+
+
+Logger& getLogger() {
+    static Logger* logger = new Logger(modInfo);
+    return *logger;
+}
+
+
+extern "C" void setup(ModInfo& info) {
+    info.id = ID;
+    info.version = VERSION;
+    modInfo = info;
+	
+    getConfig().Load(); 
+    getLogger().info("Completed setup!");
+}
+
+
+extern "C" void load() {
+    il2cpp_functions::Init();
+
+    getLogger().info("Installing hooks...");
+    
+    getLogger().info("Installed all hooks!");
+}
